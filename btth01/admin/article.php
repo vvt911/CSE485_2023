c0e2ad10a5a5bfe6d7be8f35639dff02cc093000
vvt911 <thuyvucuber@gmail.com>
2023-02-23 17:13:20 +0700

    edit article

--- btth01/admin/article.php
+++ btth01/admin/article.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music for Life</title>
+    <title>Bài viết</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style_login.css">
@@ -79,7 +79,7 @@
                                     <td><?php echo $row['ten_tloai']; ?></td>
                                     <td><?php echo $row['ten_tgia']; ?></td>
                                     <td>
-                                        <a href="edit_category.php?id=<?php echo $row["ma_bviet"] ?>"><i class="fa-solid fa-pen-to-square"></i></a>
+                                        <a href="edit_article.php?id=<?php echo $row["ma_bviet"] ?>"><i class="fa-solid fa-pen-to-square"></i></a>
                                     </td>
                                     <td>
                                         <a href=""><i class="fa-solid fa-trash"></i></a>
